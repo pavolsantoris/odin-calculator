1180681fd9d4828153ceb8304839406e18453259
pavol-me <santoris.paul@pm.me>
2022-03-09 09:40:39 +0100

    Add support for chaining calculations, it's no longer necessary to press = after every pair of numbers

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const display = document.getElementById('display');
-const oneBtn = document.getElementById('number1');
-const twoBtn = document.getElementById('number2');
-const threeBtn = document.getElementById('number3');
-const fourBtn = document.getElementById('number4');
-const fiveBtn = document.getElementById('number5');
-const sixBtn = document.getElementById('number6');
-const sevenBtn = document.getElementById('number7');
-const eightBtn = document.getElementById('number8');
-const nineBtn = document.getElementById('number9');
-const zeroBtn = document.getElementById('number0');
-const plusBtn = document.getElementById('plus');
-const minusBtn = document.getElementById('minus');
-const multiplyBtn = document.getElementById('multiply');
-const divideBtn = document.getElementById('divide');
+const memory = document.getElementById('memory');
+const numbers = document.querySelectorAll('.numbers');
+// const oneBtn = document.getElementById('number1');
+// const twoBtn = document.getElementById('number2');
+// const threeBtn = document.getElementById('number3');
+// const fourBtn = document.getElementById('number4');
+// const fiveBtn = document.getElementById('number5');
+// const sixBtn = document.getElementById('number6');
+// const sevenBtn = document.getElementById('number7');
+// const eightBtn = document.getElementById('number8');
+// const nineBtn = document.getElementById('number9');
+// const zeroBtn = document.getElementById('number0');
+// const plusBtn = document.getElementById('plus');
+// const minusBtn = document.getElementById('minus');
+// const multiplyBtn = document.getElementById('multiply');
+// const divideBtn = document.getElementById('divide');
 const equalBtn = document.getElementById('equals');
 const clearBtn = document.getElementById('clear');
+const operatorBtns = document.querySelectorAll('.operator');
+
+let operator = '';
+let a = '';
+let b = '';
 
 function operate(a, b, operator) {
      switch (operator) {
@@ -42,12 +49,42 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-     return a / b;
+     return ((a == 0 || b == 0) ? 'XXX' : a / b);
 }
 
 function print(key) {
      display.textContent += key;
 }
 
-oneBtn.addEventListener('click', print(oneBtn.textContent));
-twoBtn.addEventListener('click', print(twoBtn.textContent));
+numbers.forEach(number => number.addEventListener('click', function(e) {print(this.textContent);}));
+
+operatorBtns.forEach(operatorBtn => operatorBtn.addEventListener('click', function(e) {
+     if (a === '') {
+          a = parseInt(display.textContent);
+          operator = this.textContent;
+          memory.textContent = a + ' ' + operator;
+          display.textContent = '';
+     } else {
+          b = parseInt(display.textContent);
+          display.textContent = '';
+          a = operate(a, b, operator);
+          b = '';
+          operator = this.textContent;
+          memory.textContent = a + ' ' + operator;
+     }
+}));
+
+equalBtn.addEventListener('click', function() {
+     b = parseInt(display.textContent);
+     memory.textContent += ' ' + display.textContent;
+     display.textContent = operate(a, b, operator);
+     b = '';
+     a = '';
+});
+
+clearBtn.addEventListener('click', function() {
+     display.textContent = '';
+     memory.textContent = '';
+     a = '';
+     b = '';
+});
